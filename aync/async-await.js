f45ded39275c4dbdb8c8274c3ad91ad2ca69e6ed
Themispl <themis.pilarinos@gmail.com>
2024-07-19 20:58:48 +0300

    vue exmpl

--- aync/async-await.js
+++ aync/async-await.js
@@ -1,57 +1,63 @@
-$(function(){
-    showProducts();
+$(function() {
+    fetchAndShowProducts()
 })
 
-async function showProducts(){
-    try{
-        const response = await fetchAllProducts();
-        handleResponse(response);
-    }catch(error){
-        console.log(error);
+async function fetchAndShowProducts() {
+    try {
+        const response = await fetchAllProducts()
+        handleResponse(response)
+    } catch (error) {
+        console.log(error)
     }
 }
 
-async function fetchAllProducts(){
-    const url = await fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json');
-    return await getXHRPromise(url);
+async function fetchAllProducts() {
+    const url = 'https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json'
+    return await getXHRPromise(url)
 }
 
-function getXHRPromise(url){
-        return new Promise((resolve, reject) => {
-        let ajaxRequest = new XMLHttpRequest();
-        ajaxRequest.open('GET', url,true)
+function getXHRPromise(url) {
+    return new Promise((resolve, reject) => {
+        let ajaxRequest = new XMLHttpRequest()
+        ajaxRequest.open('GET', url, true)
 
         ajaxRequest.timeout = 5000
         ajaxRequest.ontimeout = () => onAPIError()
 
-        ajaxRequest.onreadystatechange = function(){
-            if(ajaxRequest.readyState === 4){
-                if(ajaxRequest.status === 200){
-                    const products = JSON.parse(ajaxRequest.responseText);
-                    resolve(products);
-                }else{
-                    reject(new Error(`Error einai: ${ajaxRequest.status}`));
+        ajaxRequest.onreadystatechange = function() {
+            if (ajaxRequest.readyState === 4) {
+                if (ajaxRequest.status === 200) {
+                    const products = JSON.parse(ajaxRequest.responseText)
+                    resolve(products)
+                } else {
+                    reject(new Error('Failed to load the data'))
                 }
             }
         }
-        ajaxRequest.send();
+        ajaxRequest.send()
     })
 }
 
-function handleResponse(results){
-    if(!results) return;
-    buildProducts(results);
+function handleResponse(results) {
+    if (!results) return
+
+    buildProducts(results)
+} 
+
+function buildProducts(products) {
+    const transformedProducts = products.map(({ name: proName, price, type }) => {
+        return `<div>
+                    <div><strong>Name: </strong>${proName}</div>
+                    <div><strong>Price: </strong>${price}</div>
+                    <div><strong>Type: </strong>${type}</div>
+                </div>
+                <hr>`
+    })
+
+    const productsHTML = transformedProducts.join('')
+    $('#prodList').html(productsHTML)
 }
 
-function buildProducts(products){
-    const trabsformedProducts = products.map(({name: proName, price, type}) => {
-        return `<div><strong>Name:</strong> ${proName}</div>
-        <div><strong>Price:</strong> ${price}</div>
-        <div><strong>type:</strong> ${type}</div>`
-    });
-    $('#prodList').html(trabsformedProducts.join(''));
+function onAPIError() {
+    console.log('API Error')
 }
-function onAPIError(){
-    console.log('API Error');
-}
-
